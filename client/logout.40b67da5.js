2b02b2dd6eb94fa0d47fadeb83dca821b2c543e8
Farolan Faisal <farolanfaisal@gmail.com>
2020-10-18 21:47:14 +0800

    update

--- client/logout.40b67da5.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as a,a1 as e,a2 as r}from"./client.79bce577.js";function n(s){return e((()=>r())),[]}export default class extends s{constructor(s){super(),t(this,s,n,null,a,{})}}
