c32f0e86ba7467d73481619cd735ca9af9b31bd7
Farolan Faisal <farolanfaisal@gmail.com>
2020-10-21 06:45:02 +0800

    update

--- client/logout.0d4d169d.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as a,a8 as r,a9 as e}from"./client.6b6df5c9.js";function n(s){return r((()=>e())),[]}export default class extends s{constructor(s){super(),t(this,s,n,null,a,{})}}
---
+++ client/logout.f2fdf23a.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as a,a8 as e,a9 as r}from"./client.7482df7e.js";function n(s){return e((()=>r())),[]}export default class extends s{constructor(s){super(),t(this,s,n,null,a,{})}}
